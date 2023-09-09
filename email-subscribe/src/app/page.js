64920d13ecf1f858f55f952fffc24c2affebaf80
Nicolas Korsunski <84050410+letmegro@users.noreply.github.com>
2023-09-09 16:13:44 -0400

    Update page.js

--- email-subscribe/src/app/page.js
+++ email-subscribe/src/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import food from './imgs/image.jpg'
 
 async function sendMail(em){
-    await fetch("pages/api/nodemailer", {
+    await fetch("nodemailer", {
         method: 'POST',
         body: JSON.stringify({ Email : em}) //em contains name of email that is sent in
     });
