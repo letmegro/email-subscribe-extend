c09991995920699058eb9ef1d3e3d347c7c25204
Nicolas Korsunski <84050410+letmegro@users.noreply.github.com>
2023-10-22 17:00:40 -0400

    Update nodemailer.js
    changed from a depreciated SSLv3 to tls 1.2/tls 1.3

--- email-subscribe/pages/api/nodemailer.js
+++ email-subscribe/pages/api/nodemailer.js
@@ -1,12 +1,19 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: "hotmail",
-    port: 587,
+    port: 465,
+    requireTLS: true,
     auth: {
         user: process.env.NEXT_ESERVER,
         pass: process.env.NEXT_KEY //add password here when to use
     },
-    tls: { ciphers: 'SSLv3' }
+    
+    tls: { 
+        maxVersion: 'TLSv1.3',
+        minVersion: 'TLSv1.2',
+        ciphers: 'TLS_AES_128_GCM_SHA256'
+    }
+    
 });
 
 
@@ -15,7 +22,7 @@ export default async function handler(req, res){
     const email = body['Email'];
     const options = {
         from: process.env.NEXT_ESERVER,
-        to: email,
+        to: email.toString(),
         subject: "Diet and Fitness newletter",
         text: "Dear subscriber\n\nThank you for testing out this email subscription.\n\n\nPlease note this is a mini project and not a real subscription service\nPlease do not reply to this email."
     };
@@ -29,6 +36,5 @@ export default async function handler(req, res){
      })
     });
     res.send('Status: 200');
-    console.log("sent");
     
 }
